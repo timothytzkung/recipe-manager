403bd1ecc9f0e0a378ac4498d52f29b44d542f6b
Tim Kung <timothytzkung@gmail.com>
2026-01-29 12:34:15 -0800

    Update App.jsx
    Formatting

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import { HomeView, DetailsView, AddRecipeView } from './views'
 import { Navbar } from "./components/Navbar"
 
 function App() {
+  // States
+  const [currentView, setCurrentView] = useState(HomeView);
+  const [recipes, setRecipes] = useState(null);
+  const [selectedRecipeId, setSelectedRecipeId] = useState(0);
+
+  // View Object for components
   const views = {
     Home: HomeView, 
     Details: DetailsView, 
     AddRecipe: AddRecipeView
   }
 
-  const [currentView, setCurrentView] = useState(HomeView);
-  const [recipes, setRecipes] = useState(null);
-  const [selectedRecipeId, setSelectedRecipeId] = useState(0);
 
   return (
     <>
